17e177f1b3923c6a69dae1a358ecf443a9bfacfc
gibriam.ovando <gibriam@gmail.com>
2021-05-04 13:25:08 -0500

    Commit 03052021

---
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ProductsService} from '../../../core/services/products/products.service';
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.scss']
+})
+export class FormProductComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private productsService: ProductsService,
+              private router: Router) 
+  {
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      id:['',Validators.required],
+      title:['',Validators.required],
+      price:['',Validators.required],
+      image:[''],
+      description:['',Validators.required]
+    })
+  }
+
+  saveProduct(event:Event){
+    event.preventDefault();
+    if(this.form.valid){
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((response) => {
+        console.log(response);
+        this.router.navigate(['./admin/products'])
+      })
+    }
+    console.log(this.form.value)
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
 import { ExponentialPipe } from './../shared/pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './../shared/directives/highlight/highlight.directive';
 import { HeaderComponent } from './../shared/components/header/header.component';
@@ -17,7 +18,8 @@ import {MaterialModule} from './../material/material.module';
   imports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   exports: [
     ExponentialPipe,
